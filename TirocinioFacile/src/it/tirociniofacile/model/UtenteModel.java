89c431ef8adbe0cdc87ca72d2dca96fe6740b7bf
PaoloDC <PaoloDC@users.noreply.github.com>
2017-12-26 11:38:55 +0100

    aggiunta classi

--- TirocinioFacile/src/it/tirociniofacile/model/UtenteModel.java
+++ TirocinioFacile/src/it/tirociniofacile/model/UtenteModel.java
@@ -1,10 +1,5 @@
 package it.tirociniofacile.model;
 
 public class UtenteModel {
-	/**
-	 * ciao eccomi qua sto modificando
-	 * modifica delle 11 e 28
-	 * ulteriore modifica delle 11 e 32
-	 * #saddasasdasd
-	 */
+
 }
