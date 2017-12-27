e4b5b50845197d7983ec7d5f586d2fcac9e24ed5
TheIvan96 <ivanprintf@gmail.com>
2017-12-27 10:56:19 +0100

    in 7 si sta bene

--- TirocinioFacile/src/it/tirociniofacile/control/GestioneRicercaTirocinio.java
+++ TirocinioFacile/src/it/tirociniofacile/control/GestioneRicercaTirocinio.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/GestioneRicercaTirocinio")
 public class GestioneRicercaTirocinio extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
     /**
      * Default constructor. 
