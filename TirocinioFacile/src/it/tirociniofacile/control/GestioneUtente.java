a8b325baaa904161915230c20230bd6cf10db8f5
PaoloDC <PaoloDC@users.noreply.github.com>
2017-12-27 10:47:02 +0100

    gestione utente

--- TirocinioFacile/src/it/tirociniofacile/control/GestioneUtente.java
+++ TirocinioFacile/src/it/tirociniofacile/control/GestioneUtente.java
@@ -12,30 +12,25 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/GestioneUtente")
 public class GestioneUtente extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
        
-    /** .
+  /** .
      * @see HttpServlet#HttpServlet()
      */
-    public GestioneUtente() {
+  public GestioneUtente() {
         super();
         // TODO Auto-generated constructor stub
-    }
+  }
 
-	/** .
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws ServletException, IOException {
+    response.getWriter().append("Served at: ").append(request.getContextPath());
+  }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+      throws ServletException, IOException {
+    doGet(request, response);
+  }
 
 }
