3845179dee52a2f125f723b16bd8c672821bcd30
TheIvan96 <ivanprintf@gmail.com>
2018-01-05 11:26:13 +0100

    modifica nella servlet gestione utente

--- TirocinioFacile/src/it/tirociniofacile/control/GestioneUtente.java
+++ TirocinioFacile/src/it/tirociniofacile/control/GestioneUtente.java
@@ -41,8 +41,7 @@ public class GestioneUtente extends HttpServlet {
   
   protected void doGet(HttpServletRequest request, HttpServletResponse response) 
       throws ServletException, IOException {
-    response.getWriter().append("Served at: ").append(request.getContextPath());
-   
+    doPost(request,response);
   }
 
   /**
