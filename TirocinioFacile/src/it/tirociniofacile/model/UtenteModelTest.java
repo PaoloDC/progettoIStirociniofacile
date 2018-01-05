856ab20d7946986aa6cd72640d958ce5f50a7f92
Andreasessa1995 <32262855+Andreasessa1995@users.noreply.github.com>
2018-01-05 12:21:01 +0100

    Test ancora da sviluppare

---
+++ TirocinioFacile/src/it/tirociniofacile/model/UtenteModelTest.java
@@ -0,0 +1,12 @@
+package it.tirociniofacile.model;
+import junit.framework.*;
+
+public class UtenteModelTest extends TestCase {
+
+  public UtenteModelTest() {
+    // TODO Auto-generated constructor stub
+  }
+
+ 
+
+}
