dc59d690038f647ef502f8c4659a5601855b301c
PaoloDC <PaoloDC@users.noreply.github.com>
2018-01-06 10:45:08 +0100

    creata classe di test utente model

---
+++ TirocinioFacile/src/it/tirociniofacile/model/TestUtenteModel.java
@@ -0,0 +1,36 @@
+package it.tirociniofacile.model;
+
+import junit.framework.TestCase;
+
+public class TestUtenteModel extends TestCase {
+
+  UtenteModel model = new UtenteModel();
+  
+  public void testSalvaAccountStudente() {
+    
+  }
+  
+  public void testGeneraCredenziali() {
+    
+  }
+ 
+  public void testCaricaUtentiDaFile() {
+    
+  }
+  
+  public void testSalvaUtentiNelFile() {
+    
+  }
+  
+  public void testCaricaAccount() {
+    
+  }
+  
+  public void testCercaAccountPerEmail() {
+    
+  }
+  
+  public static void main(String[] args) {
+    
+  }
+}
