c537c77e1e47e082f245bba15a19d0ccab95c1c4
PaoloDC <PaoloDC@users.noreply.github.com>
2018-01-27 19:05:25 +0100

    fix righe vuote

--- TirocinioFacile/src/it/tirociniofacile/model/DocumentoModel.java
+++ TirocinioFacile/src/it/tirociniofacile/model/DocumentoModel.java
@@ -18,8 +18,6 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
-
-
 /**
  * Classe model per la gestione di lettura e scrittura dei documenti (convenzione e questionario)
  * sul database.
